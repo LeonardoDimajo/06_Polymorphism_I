13fb7b454af1ee08c9c6a069c3026127f18eac3e
LeonardoDimajo <joscha.meurer@hotmail.de>
2024-07-19 12:06:36 +0200

    03 | 3 Iteration

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,15 +5,42 @@ public class App {
     public static void main(String[] args) {
      
         
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        pollObj(queen);
+
+        pollObj(worker);
+
+        pollObj(drone);
+        
     }
 
-   
+    // Overloading
+   private static void pollObj(Queen obj){
+    output(obj.doYourJob());
+    output(obj.fly());
+    output("--------------");
+   }
+
+   // Overloading
+   private static void pollObj(Worker obj){
+    output(obj.doYourJob());
+    output(obj.fly());
+    output("--------------");
+   }
+
+   // Overloading
+    private static void pollObj(Drone obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("--------------");
+    }
    
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
 
 }
 
